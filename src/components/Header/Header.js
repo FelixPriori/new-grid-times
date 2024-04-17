5b47e96564fd2fd9568ef94276ab2d10cc39d97a
Felix Rioux Sabourin <53023071+FelixPriori@users.noreply.github.com>
2024-04-17 15:19:46 +0000

    finish exercise 1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { QUERIES } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
   return (
@@ -31,6 +31,21 @@ const Header = () => {
       <MainHeader>
         <Logo />
       </MainHeader>
+      <DesktopHeader>
+        <DesktopActions>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </DesktopActions>
+        <Logo />
+        <SubscribeWrapper>
+          <Button>subscribe</Button>
+          <DesktopLink>Already a subscriber?</DesktopLink>
+        </SubscribeWrapper>
+      </DesktopHeader>
     </header>
   );
 };
@@ -39,6 +54,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -65,6 +84,54 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
+`;
+
+const DesktopHeader = styled(MainHeader)`
+  display: none;
+
+  @media ${QUERIES.tabletAndUp} {
+    margin-top: 48px;
+    margin-bottom: 72px;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+    align-items: center;
+    justify-content: revert;
+    justify-items: start;
+    margin-top: 16px;
+    margin-bottom: 72px;
+  }
+`;
+
+const DesktopActions = styled(ActionGroup)`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+  }
+`;
+
+const SubscribeWrapper = styled(DesktopActions)`
+  display: revert;
+  position: relative;
+  justify-self: end;
+`;
+
+const DesktopLink = styled.a`
+  position: absolute;
+  width: 100%;
+  margin-top: 8px;
+  text-align: center;
+  font-size: ${14 / 16}rem;
+  color: var(--color-gray-900);
+  font-style: italic;
+  text-decoration: underline;
 `;
 
 export default Header;
